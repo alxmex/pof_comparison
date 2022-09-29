a3da6e0a2a6d7733e293a872fb6844715ff6375d
Alexander Mehks <54999803+alexandermehks@users.noreply.github.com>
2022-09-29 13:38:31 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use chrono::Datelike;
 use std::io::prelude::*;
 
 fn main() {
-
-
     let mut ms_ledgerx = File::open("files/ms_ledgerx.txt").expect("File not found");
     let mut ms = String::new();
     ms_ledgerx.read_to_string(&mut ms).expect("Error while reading file");
@@ -16,10 +14,9 @@ fn main() {
     let mut delta_ledgerx = File::open("files/delta_ledgerx.txt").expect("File not found");
     let mut data3 = String::new();
     delta_ledgerx.read_to_string(&mut data3).expect("Error while reading file");
+    
     changes(&mut pg, &mut ms);
 }
-
-
 
 fn changes(pg: &str, ms: &str){
     let year = chrono::Utc::now().date().year();
@@ -40,22 +37,17 @@ fn changes(pg: &str, ms: &str){
         }
     }
 
-
-
 fn contatinate_strings(v1: i32 , v2: u32 ) -> String {
     let mut year = v1.to_string();
     let month = v2.to_string();
 
-   
     year.push_str("-0");
     year.push_str(&month);
     let concated_string = year.replace('"', "'");
     concated_string
 }
 
-
-
-fn data_clean(data1: &[&str], data2:&[&str]) -> bool{
+fn data_clean(data1: &[&str], data2: &[&str]) -> bool{
     match data1 == data2{
         true => {
             true
@@ -65,13 +57,3 @@ fn data_clean(data1: &[&str], data2:&[&str]) -> bool{
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
